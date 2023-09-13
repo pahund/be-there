aa94e7ec44d644cd023210280a41bd069d70cb96
Patrick Hund <patrick.hund@adevinta.com>
2023-09-13 10:06:46 +0200

    update Jira and GitHub links to new project FRSH

--- background.js
+++ background.js
@@ -8,16 +8,16 @@ chrome.commands.onCommand.addListener((command) => {
   console.log(`Command received: ${command}`);
   switch (command) {
     case 'open_url1':
-      console.log("Navigating to open Searchlight pull requests on GitHub Enterprise");
-      navigateTo('https://github.es.ecg.tools/mobile-de/consumer-webapp/pulls?q=is%3Apr+is%3Aopen+label%3Asearchlight');
+      console.log("Navigating to open FRSH pull requests on GitHub Enterprise");
+      navigateTo('https://github.es.ecg.tools/mobile-de/mde-ui/pulls?q=is%3Apr+is%3Aopen+label%3AFRSH');
       break;
     case 'open_url2':
-      console.log("Navigating to current sprint board of project Searchlight in Jira");
-      navigateTo('https://jira.ets.mpi-internal.com/secure/RapidBoard.jspa?rapidView=3008005');
+      console.log("Navigating to Kanban board of project FRSH in Jira");
+      navigateTo('https://jira.ets.mpi-internal.com/secure/RapidBoard.jspa?rapidView=4001583');
       break;
     case 'open_url3':
-      console.log("Navigating to user story map of project Searchlight in Jira");
-      navigateTo('https://jira.ets.mpi-internal.com/projects/MC?selectedItem=com.arijea.plugins.easy-agile-user-story-maps&rapidView=3008005#/');
+      console.log("Navigating to user story map of project FRSH in Jira");
+      navigateTo('https://jira.ets.mpi-internal.com/secure/EasyAgileUserStoryMap.jspa?rapidView=4001583#/');
       break;
     default:
       console.error(`Unknown command ${command}`);
