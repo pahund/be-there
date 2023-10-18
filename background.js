a0c4e7cdf89a9f15cd2dc543653ed9b8e78c7e66
Patrick Hund <patrick.hund@adevinta.com>
2023-10-18 08:47:58 +0200

    change MDE UI PR URL

--- background.js
+++ background.js
@@ -8,8 +8,8 @@ chrome.commands.onCommand.addListener((command) => {
   console.log(`Command received: ${command}`);
   switch (command) {
     case 'open_url1':
-      console.log("Navigating to open FRSH pull requests on GitHub Enterprise");
-      navigateTo('https://github.es.ecg.tools/mobile-de/mde-ui/pulls?q=is%3Apr+is%3Aopen+label%3AFRSH');
+      console.log("Navigating to open MDE UI pull requests on GitHub Enterprise");
+      navigateTo('https://github.es.ecg.tools/mobile-de/mde-ui/pulls?q=is%3Apr+is%3Aopen+');
       break;
     case 'open_url2':
       console.log("Navigating to Kanban board of project FRSH in Jira");
